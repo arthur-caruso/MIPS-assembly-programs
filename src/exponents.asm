7d2b30d8dd5d3c16e52cee268ffa12f359bbbf0e
arthur-caruso <101721229+arthur-caruso@users.noreply.github.com>
2022-06-14 14:11:21 -0300

    Update exponents.asm

--- src/exponents.asm
+++ src/exponents.asm
@@ -1,79 +1,43 @@
-# Autor: Arthur Francisco Caruso
-
-# o código assembly mips32 a seguir imita o comportamento
-# do código em alto nível da linguagem c a seguir:
-#	void main() {
-#		int value = 2, pow = 10;
-#		value = potencia(value, pow);
+#		int base = 2, exp = 10;
+#		int val = pow(base, exp);
 #	}
-#	int potencia(int value, int pow) {
-#		if (pow == 0) return 1;
-#		else return value * potencia(value, pow--);
+#	int pow(int base, int exp) {
+#		if (exp == 0) return 1;
+#		else return base * pow(base, exp--);
 #	}
 
 .data
-value: .word 3
-pow: .word 4
+base: .word 3
+exp: .word 4
+val: .space 4
 
 .text
-main:
-	# guardar value em a1 (primeiro argumento)
-	la $a1, value
-	lw $a1, 0($a1)
+main:	lw $v1, base
+	lw $a1, base
+	lw $a2, exp
 
-	# guardar pow em a2 (segundo argumento)
-	la $a2, pow
-	lw $a2, 0($a2)
+	jal pow
+	sw $v1, val
 
-	# guardar value em t0 (estado temporário)
-	la $t0, value
-	lw $t0, 0($t0)
-
-	# pular para subrotina potencia(value, pow)
-	jal potencia
-
-	# salvar resultado ($t0 -> value)
-	la $t1, value
-	sw $t0, 0($t1)
-
-	# imprimir resultado (apenas para testes)
-	move $a0, $t0
+	lw $a0, val
 	li $v0, 1
 	syscall
 
-	# sair do programa
 	li $v0, 10
 	syscall
 
-potencia:
-	# ajustar stack pointer
-	addi $sp, $sp, -8
-
-	# salvar conteúdo do registrador ($a2)
+pow:	addi $sp, $sp, -8
 	sw $a2, 4($sp)
-
-	# salvar o endereço de retorno ($ra)
 	sw $ra, 0($sp)
 
-	# decrementar pow (pow--)
 	addi $a2, $a2, -1
-
-	# sair quando pow for zero ($a2 == 0 ? exit : potencia)
 	beq $a2, $zero, exit
 
-	# chamada recursiva
-	jal potencia
+	jal pow
+	mul $v1, $a1, $v1
 
-	# $t0 <- value * potencia(value, pow - 1)
-	mul $t0, $a1, $t0
-
-exit:
-	# restaurar registradores da pilha
-	lw $a2, 4($sp)
+exit:	lw $a2, 4($sp)
 	lw $ra, 0($sp)
-
-	# reajustar stack pointer
 	addi $sp, $sp, 8
 
-	# retornar para endereço de retorno ($ra)
 	jr $ra
